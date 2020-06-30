2f62ed239ea5b7df55bab85ea993ab3fcaf99aed
OMR5221 <oswaldr@live.com>
2020-06-30 12:11:43 -0400

    Added logic to send to rand src and target customers

--- generator/transactions.py
+++ generator/transactions.py
@@ -8,11 +8,11 @@ import numpy as np
 account_chars: str = digits + ascii_letters
 
 
-def _random_account_nbr(acct_nbrs) -> str:
+def _random_customer(cust_dtls) -> tuple:
     """Return a random account number made of 12 characters."""
-    return random.choice(acct_nbrs)
+    return random.choice(cust_dtls)
 
-def _get_account_nbrs() -> List[Tuple[str, str]]:
+def _get_cust_dtls() -> List[Tuple[str, str]]:
     table_name = "user_accounts_dim"
     conn = connect(
             dbname="transactions_db",
@@ -21,14 +21,14 @@ def _get_account_nbrs() -> List[Tuple[str, str]]:
             password="postgres"
             )
     cursor = conn.cursor()
-    acct_nbrs_sql = f'SELECT account_number, spend_type FROM {table_name}'
-    cursor.execute(acct_nbrs_sql)
-    acct_nbrs = cursor.fetchall()
-    return acct_nbrs
+    cust_dtls_sql = f'SELECT account_number, spend_type FROM {table_name}'
+    cursor.execute(cust_dtls_sql)
+    cust_dtls = cursor.fetchall()
+    return cust_dtls
 
 def _random_amount(spend_type) -> float:
     # Get a bucket the user will choose to spend the amount from:
-    bucket_probs = {0: [0.8, 0.1, 0.1], 1: [0.3,0.5, 0.2], 2: [0.2,0.4,0.4]}
+    bucket_probs = {0: [0.8, 0.1, 0.1], 1: [0.3, 0.5, 0.2], 2: [0.2, 0.4, 0.4]}
     bucket_num = np.random.choice(3, 1, p=bucket_probs[spend_type])
     
     if bucket_num == 0:
@@ -44,11 +44,14 @@ def _random_amount(spend_type) -> float:
 
 def create_random_transaction() -> dict:
     """Create a fake, randomised transaction."""
-    acct_nbrs = _get_account_nbrs()
+    cust_dtls = _get_cust_dtls()
+    source_cust = _random_customer(cust_dtls)
+    rand_amt = _random_amount(rand_cust[1])
+    target_cust = _random_customer(cust_dtls)
+
     return {
-        'source': _random_account_nbr(acct_nbrs),
-        'target': _random_account_nbr(acct_nbrs),
-        'amount': _random_amount(),
-        # Keep it simple: it's all euros
-        'currency': 'EUR',
+        'source': source_cust[0],
+        'target': target_cust[0],
+        'amount': rand_amt,
+        'currency': 'USD',
     }
